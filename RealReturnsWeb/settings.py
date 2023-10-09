71735913bc1b1c715babf4afe165ffde7a4f5571
sai prathap reddy <prathap.r@paccore.com>
2023-10-09 12:28:12 +0530

    minor change

--- RealReturnsWeb/settings.py
+++ RealReturnsWeb/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": "db",
+        "HOST": "localhost",
         "PORT": "3306",
     }
 }
