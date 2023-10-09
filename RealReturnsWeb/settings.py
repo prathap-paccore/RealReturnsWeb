dd4e262461435dd0b88707e21ed5868643f4bf3c
sai prathap <126855218+prathap-paccore@users.noreply.github.com>
2023-10-09 20:21:52 +0530

    Update settings.py

--- RealReturnsWeb/settings.py
+++ RealReturnsWeb/settings.py
@@ -145,11 +145,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = "/static/static/"
-MEDIA_URL = "/static/media/"
+# STATIC_URL = "/static/static/"
+# MEDIA_URL = "/static/media/"
 
-MEDIA_ROOT = "/vol/web/media"
-STATIC_ROOT = "/vol/web/static"
+# MEDIA_ROOT = "/vol/web/media"
+# STATIC_ROOT = "/vol/web/static"
+
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
